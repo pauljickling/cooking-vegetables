464bb9135140b560169f8bc21faa2ffa4eef0e3c
Paul Jickling <pjickling@gmail.com>
2019-01-02 08:51:57 -0800

    Configured hypothetical grid layout

--- src/Chart.js
+++ src/Chart.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 const Chart = () => (
     <div className="chart">
-
+      <div></div>
+      <p>Blanch</p>
+      <p>Saute</p>
+      <p>Steamy Saute</p>
+      <p>Roast</p>
+      <p>Long-Cook</p>
+      <p>Grill</p>
+      <p>Artichokes</p>
     </div>
 );
 
