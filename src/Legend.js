458fb0424c8258df484eca3d46dbc1012fa7159e
Paul Jickling <pjickling@gmail.com>
2019-01-02 09:06:52 -0800

    Added color codes to legend component

--- src/Legend.js
+++ src/Legend.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const Legend = () => (
   <div className="legend">
-    <p>Spring - Summer - Fall - Winter</p>
+    <p>Spring <div className="spring"></div>  Summer <div className="summer"></div>  Fall <div className="fall"></div>  Winter <div className="winter"></div></p>
   </div>
 );
 
