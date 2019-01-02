15fa5865ba149daeb9ebeb0ba04148d1e6cc3394
Paul Jickling <pjickling@gmail.com>
2019-01-02 08:43:43 -0800

    Changed chart component from table to grid display

--- src/Chart.js
+++ src/Chart.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
 
 const Chart = () => (
-    <table className="chart">
-      <tr>
-        <td>Vegetable</td>
-        <td>Sautee</td>
-        <td>Roast</td>
-        <td>Boil</td>
-      </tr>
-      <tr>
-        <td>Asparaugus</td>
-        <td>Foo</td>
-        <td>Bar</td>
-        <td>Foo</td>
-      </tr>
-    </table>
+    <div className="chart">
+
+    </div>
 );
 
 export default Chart;
