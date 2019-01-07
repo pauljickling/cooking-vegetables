8f9d1c5bae17c5a666ffe5dcde8381377acf8107
Paul Jickling <pjickling@gmail.com>
2019-01-07 09:50:01 -0800

    Vegetable component now possesses pattern prop

--- src/Vegetable.js
+++ src/Vegetable.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Vegetable = (props) => [
   <div key="1">{props.name}</div>,
-  <div key="2">Spring, Fall</div>,
-  <div key="3"></div>,
-  <div key="4">Spring, Fall</div>,
-  <div key="5">Spring</div>,
-  <div key="6">Spring, Summer, Fall, Winter</div>,
-  <div key="7"></div>
+  <div key="2">{props.pattern}</div>,
+  <div key="3">{props.pattern}</div>,
+  <div key="4">{props.pattern}</div>,
+  <div key="5">{props.pattern}</div>,
+  <div key="6">{props.pattern}</div>,
+  <div key="7">{props.pattern}</div>
 ];
 
 export default Vegetable;
