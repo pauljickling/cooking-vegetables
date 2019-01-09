5562e38807c5e90c95253078c079c058912a3941
Paul Jickling <pjickling@gmail.com>
2019-01-09 10:23:48 -0800

    Pattern prop renamed seasons prop

--- src/Vegetable.js
+++ src/Vegetable.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Vegetable = (props) => [
   <div key="1">{props.name}</div>,
-  <div key="2">{props.pattern}</div>,
-  <div key="3">{props.pattern}</div>,
-  <div key="4">{props.pattern}</div>,
-  <div key="5">{props.pattern}</div>,
-  <div key="6">{props.pattern}</div>,
-  <div key="7">{props.pattern}</div>
+  <div key="2">{props.seasons}</div>,
+  <div key="3">{props.seasons}</div>,
+  <div key="4">{props.seasons}</div>,
+  <div key="5">{props.seasons}</div>,
+  <div key="6">{props.seasons}</div>,
+  <div key="7">{props.seasons}</div>
 ];
 
 export default Vegetable;
