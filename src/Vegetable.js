ec3a27d3ce6c0c6491ad97ec56b62fddf0b1bd7d
Paul Jickling <pjickling@gmail.com>
2019-01-10 14:13:50 -0800

    divided up different props

--- src/Vegetable.js
+++ src/Vegetable.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Vegetable = (props) => [
   <div key="1">{props.name}</div>,
-  <div key="2">{props.seasons}</div>,
-  <div key="3">{props.seasons}</div>,
-  <div key="4">{props.seasons}</div>,
-  <div key="5">{props.seasons}</div>,
-  <div key="6">{props.seasons}</div>,
-  <div key="7">{props.seasons}</div>
+  <div key="2">{props.blanch}</div>,
+  <div key="3">{props.saute}</div>,
+  <div key="4">{props.steamySaute}</div>,
+  <div key="5">{props.roast}</div>,
+  <div key="6">{props.longCook}</div>,
+  <div key="7">{props.grill}</div>
 ];
 
 export default Vegetable;
